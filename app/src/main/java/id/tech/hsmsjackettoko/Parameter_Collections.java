6ba4adadc13a96df5a2b7241e83328fff10a8c6f
ridho <ridho_animage@yahoo.com>
2016-03-01 19:42:39 +0700

    ubah no kirim

--- app/src/main/java/id/tech/hsmsjackettoko/Parameter_Collections.java
+++ app/src/main/java/id/tech/hsmsjackettoko/Parameter_Collections.java
@@ -4,7 +4,7 @@ package id.tech.hsmsjackettoko;
  * Created by macbook on 2/4/16.
  */
 public class Parameter_Collections  {
-    public static final String nomer_kirim = "+6281283676317";
+    public static final String nomer_kirim = "+6281280070020";
 
 
     public static final String nomer_holcim = "Holcim";
